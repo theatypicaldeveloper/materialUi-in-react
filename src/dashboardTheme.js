bc03067d8dfa21a66875ae1738644f2bc682f484
theatypicaldeveloper <mighty.matty91@gmail.com>
2021-12-21 13:57:07 +0000

    MUI - added starting point

--- src/dashboardTheme.js
+++ src/dashboardTheme.js
@@ -4,8 +4,23 @@ export const dashboardTheme = createTheme({
     components: {
       MuiButton: {
         styleOverrides: {
-            contained: {
-                fontSize: '0.875rem'
+            root: {
+                fontSize: '0.875rem',
+                fontWeight: 600,
+                borderRadius: 8.5,
+                '&.MuiButton-contained': {
+                    backgroundColor: '#009be5',
+                    '&:hover': {
+                        backgroundColor: '#006db3'
+                    },
+                },
+                '&.MuiButton-outlined': {
+                    color: "#fff",
+                    borderColor: '#fff',
+                    '&:hover': {
+                        backgroundColor: 'transparent'
+                    },
+                },
             },
         },
       },
--- src/pages/Authentication/Authentication.js
+++ src/pages/Authentication/Authentication.js
@@ -3,40 +3,18 @@ import Grid from '@mui/material/Grid';
 import CommonButton from '../../components/common/CommonButton/CommonButton';
 
 const Authentication = () => {
-    const buttonStyles = {
-        fontSize: '0.875rem',
-        fontWeight: 600,
-        textTransform: 'capitalize',
-        borderRadius: 2.5,
-        '&.MuiButton-contained': {
-            backgroundColor: '#009be5',
-            '&:hover': {
-                backgroundColor: '#006db3'
-            },
-        },
-        '&.MuiButton-outlined': {
-            color: "#fff",
-            borderColor: '#fff',
-            '&:hover': {
-                backgroundColor: 'transparent'
-            },
-        },
-    };
-
     return (
         <Grid item xs={8}>
             This is authentication page.
             <CommonButton
-                sx={buttonStyles}
                 variant="contained"
             >
                 Add user
             </CommonButton>
             <CommonButton
-                sx={buttonStyles}
                 variant="outlined"
             >
-                Add user
+                Web setup
             </CommonButton>
         </Grid>
     )
