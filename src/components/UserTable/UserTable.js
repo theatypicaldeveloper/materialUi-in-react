d2db6ab8417f578dd9f5bf8f9dc5e8394ef6e774
theatypicaldeveloper <mighty.matty91@gmail.com>
2022-02-19 11:45:06 +0000

    lesson-10 - add fetched users to table

--- src/components/UserTable/UserTable.js
+++ src/components/UserTable/UserTable.js
@@ -1,15 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import DataTable from '../common/DataTable/DataTable';
 
-const rows = [
-    { id: 1, col1: 'Hello', col2: 'World' },
-    { id: 2, col1: 'DataGridPro', col2: 'is Awesome' },
-    { id: 3, col1: 'MUI', col2: 'is Amazing' },
-];
-  
 const columns = [
-    { field: 'col1', headerName: 'Column 1', width: 150 },
-    { field: 'col2', headerName: 'Column 2', width: 150 },
+    { field: 'id', headerName: 'User ID', width: 150 },
+    { field: 'name', headerName: 'Name', width: 150 },
+    { field: 'username', headerName: 'Username', width: 150 },
+    { field: 'email', headerName: 'E-mail', width: 150 },
 ];
 
 const UserTable = () => {
@@ -21,11 +17,9 @@ const UserTable = () => {
             .then((json) => setUsers(json))
     }, []);
 
-    console.log(users)
-      
     return (
-        <DataTable 
-            rows={rows}
+        <DataTable
+            rows={users}
             columns={columns}
         />
     );
